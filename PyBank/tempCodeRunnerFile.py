7e845d2c3c5c167f83ba115c8a979cf771ccc51a
marez111 <myrealgeorgia@gmail.com>
2019-03-13 19:55:30 -0400

    finished scripts that calculate project required variables, still need cleanup

--- PyBank/tempCodeRunnerFile.py
+++ PyBank/tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-  # for row in csvreader:
-  #   balance += int(row[1])
-  #   months.append(row[0].split("-"))
-  #   if int(row[1]) > 0:
-  #    profits.append(int(row[1]))
-  #   else:
-  #     losses.append(int(row[1]))
+sum(pl_difference)
