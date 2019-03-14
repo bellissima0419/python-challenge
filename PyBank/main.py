47e36d2d14b2607685850afd9198b40be3bd6a46
marez111 <myrealgeorgia@gmail.com>
2019-03-13 21:00:53 -0400

    finished financial analysis report, todo: add month to greatest increase/decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,60 +3,42 @@ import os
 
 csvpath = os.path.join('../', 'budget_data.csv')
 
-profit_loss = []
-pl_difference = []
-profits = []
-losses = []
-p_count = 0
-l_count = 0
+profit_and_losses = []
+profit_loss_diff = []
 balance = 0
 months = []
-csv_list = []
 
 with open(csvpath, newline='') as csvfile:
 
   csvreader = csv.reader(csvfile, delimiter=',')
   csv_header = next(csvfile)
 
-  # csv_list.append(list(csvreader))
   print(f"Header: {csv_header}")
 
   for row in csvreader:
-    profit_loss.append(int(row[1]))
-    # difference.append(row[1])
+    profit_and_losses.append(int(row[1]))
+
     balance += int(row[1])
 
     months.append(row[0].split("-"))
-    if int(row[1]) > 0:
-     profits.append(int(row[1]))
-    else:
-      losses.append(int(row[1]))
 
 
-for i in range(len(profit_loss)-1):
-  pl_difference.append(profit_loss[i+1] - profit_loss[i])
+for i in range(len(profit_and_losses)-1):
+  profit_loss_diff.append(profit_and_losses[i+1] - profit_and_losses[i])
 
-print()
+avg = round(sum(profit_loss_diff)/len(profit_loss_diff),2)
+greatest_increase_profits = max(profit_loss_diff)
+greatest_decrease_profits = min(profit_loss_diff)
+total  = sum(profit_and_losses)
 
-pl_sum = 0
-for i in range(len(pl_difference)):
-  pl_sum += pl_difference[i]
+def financial_report(*args):
+  print("Financial Analysis")
+  print("--------------------------")
+  print(f"Total Months: {len(months)} ")
+  print(f"Total: ${total}")
+  print(f"Average Change: ${avg}")
+  print(f"Gratest Increase in Profits: (${greatest_increase_profits})")
+  print(f"Greatest Decrease in Profits: (${greatest_decrease_profits})")
 
-print(f"pl_sum: {pl_sum}")
-print(f"len( pl_difference ) {len(pl_difference)}")
-print(f"avg: {sum(pl_difference)/len(pl_difference)}" )
-
-print(f"max pl_difference: {max(pl_difference)}")
-print(f"min pl_difference: {min(pl_difference)}")
-
-
-
-
-
-
-
-
-
-
-
+financial_report(months, total, avg, greatest_increase_profits, greatest_decrease_profits)
 
