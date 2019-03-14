279dc7a7c91a51a801b602a18cd0cf028e5297c3
marez111 <myrealgeorgia@gmail.com>
2019-03-14 15:04:26 -0400

    Greatest increase decrease with lambda

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,43 +2,60 @@ import csv
 import os
 
 csvpath = os.path.join('../', 'budget_data.csv')
-
 profit_and_losses = []
 profit_loss_diff = []
 balance = 0
 months = []
+# csv_list = []
 
 with open(csvpath, newline='') as csvfile:
 
   csvreader = csv.reader(csvfile, delimiter=',')
   csv_header = next(csvfile)
+  csv_list = [[row[0], int(row[1])] for row in csvreader]
 
   print(f"Header: {csv_header}")
 
   for row in csvreader:
+
     profit_and_losses.append(int(row[1]))
-
     balance += int(row[1])
-
     months.append(row[0].split("-"))
 
 
-for i in range(len(profit_and_losses)-1):
-  profit_loss_diff.append(profit_and_losses[i+1] - profit_and_losses[i])
+for i in range(1, len(csv_list)):
 
-avg = round(sum(profit_loss_diff)/len(profit_loss_diff),2)
-greatest_increase_profits = max(profit_loss_diff)
-greatest_decrease_profits = min(profit_loss_diff)
-total  = sum(profit_and_losses)
+    profit_loss_diff.append([csv_list[i][0], csv_list[i][1] - csv_list[i-1][1]])
+    # print(profit_loss_diff)
+# print(profit_loss_diff)
+# print(max(profit_loss_diff[1]))
 
-def financial_report(*args):
-  print("Financial Analysis")
-  print("--------------------------")
-  print(f"Total Months: {len(months)} ")
-  print(f"Total: ${total}")
-  print(f"Average Change: ${avg}")
-  print(f"Gratest Increase in Profits: (${greatest_increase_profits})")
-  print(f"Greatest Decrease in Profits: (${greatest_decrease_profits})")
+print(max(csv_list, key=lambda x: x[1]))
+print(min(csv_list, key=lambda x: x[1]))
 
-financial_report(months, total, avg, greatest_increase_profits, greatest_decrease_profits)
+
+# print([max(row[1]) for row in profit_loss_diff if  ])
+# for i in range(len(profit_and_losses)-1):
+#   profit_loss_diff.append(profit_and_losses[i+1] - profit_and_losses[i])
+
+# print(f"profit_loss_diff: {profit_loss_diff[:-1]}")
+# print(len(profit_loss_diff))
+# print(f"csv_list: {(csv_list)}")
+# print(f"csv_list: {len(csv_list)}")
+
+# avg = round(sum(profit_loss_diff)/len(profit_loss_diff),2)
+# greatest_increase_profits = max(profit_loss_diff)
+# greatest_decrease_profits = min(profit_loss_diff)
+# total  = sum(profit_and_losses)
+
+# def financial_report(*args):
+#   print("Financial Analysis")
+#   print("--------------------------")
+#   print(f"Total Months: {len(months)} ")
+#   print(f"Total: ${total}")
+#   print(f"Average Change: ${avg}")
+#   print(f"Gratest Increase in Profits: (${greatest_increase_profits})")
+#   print(f"Greatest Decrease in Profits: (${greatest_decrease_profits})")
+
+# financial_report(months, total, avg, greatest_increase_profits, greatest_decrease_profits)
 
--- PyBank/tempCodeRunnerFile.py
+++ PyBank/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-sum(pl_difference)
+for i in range(len(csv_list)-1):
+#   profit_loss_diff.append(csv_list[i+1][1] - csv_list[i][1])
