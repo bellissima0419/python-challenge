87eff8d1e8a97f1c9f07db3be19bb0d7bbffddd8
marez111 <myrealgeorgia@gmail.com>
2019-03-15 20:53:37 -0400

    finished financial analysis. TODO: export to csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,73 +6,43 @@ profit_and_losses = []
 profit_loss_diff = []
 balance = 0
 months = []
-# csv_list = []
+csv_list = []
 
 with open(csvpath, newline='') as csvfile:
 
-  csvreader = csv.reader(csvfile, delimiter=',')
-  csv_header = next(csvfile)
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvfile)
+    # print(f"Header: {csv_header}")
+    for row in csvreader:
 
-  # cast the file into a python list making the ProfitLoses an int
-  csv_list = [[row[0], int(row[1])] for row in csvreader]
-  print(f"Header: {csv_header}")
-
-  print(f"csv_list: {csv_list[:5]}")
-
-
-  for row in csvreader:
-
-    profit_and_losses.append(int(row[1]))
-    balance += int(row[1])
-    months.append(row[0].split("-"))
+        profit_and_losses.append(int(row[1]))
+        balance += int(row[1])
+        # save the months into a list
+        months.append(row[0].split("-"))
+    # cast the file into a python list making the ProfitLoses an int
+        csv_list.append([row[0], int(row[1])])
 
 
 for i in range(1, len(csv_list)):
+        # calculate the  profit change and append it along with its  month
+    profit_loss_diff.append(
+        [csv_list[i][0], csv_list[i][1] - csv_list[i - 1][1]])
 
-    profit_loss_diff.append([csv_list[i][0], csv_list[i][1] - csv_list[i-1][1]])
-    # print(profit_loss_diff)
-print(profit_loss_diff[:5])
-# print(max(profit_loss_diff[1]))
+# calculate the max increase in profits
+greatest_profit_increase = max(profit_loss_diff, key=lambda x: x[1])
+# calculate the max decrease in profits
+greatest_profit_decrease = min(profit_loss_diff, key=lambda x: x[1])
+# calculate the average in profit change
+avg = round(sum([x[1] for x in profit_loss_diff]) / len(profit_loss_diff), 2)
 
-greatest_profit_increase = max(csv_list, key=lambda x: x[1])
-greatest_profit_decrease = min(csv_list, key=lambda x: x[1])
-
-
-avg = round(sum([x[1] for x in profit_loss_diff])/len(profit_loss_diff),2)
-
-print('avg', {avg})
-print(f"(greatest_profit_increase): {(greatest_profit_increase)}")
-print(f"greatest_profit_decrease: {greatest_profit_decrease}")
-# print(f"avg: {avg}")
-
-
-
-# print([max(row[1]) for row in profit_loss_diff if  ])
-# for i in range(len(profit_and_losses)-1):
-#   profit_loss_diff.append(profit_and_losses[i+1] - profit_and_losses[i])
-
-# print(f"profit_loss_diff: {profit_loss_diff[:-1]}")
-# print(len(profit_loss_diff))
-# print(f"csv_list: {(csv_list)}")
-# print(f"csv_list: {len(csv_list)}")
-
-
-# ========================================================
-# ========================================================
-
-# avg = round(sum(profit_loss_diff)/len(profit_loss_diff),2)
-# # greatest_increase_profits = max(profit_loss_diff)
-# # greatest_decrease_profits = min(profit_loss_diff)
-# total  = sum(profit_and_losses)
-
-# def financial_report(*args):
-#   print("Financial Analysis")
-#   print("--------------------------")
-#   print(f"Total Months: {len(months)} ")
-#   print(f"Total: ${total}")
-#   print(f"Average Change: ${avg}")
-#   print(f"Gratest Increase in Profits: (${greatest_profit_increase})")
-#   print(f"Greatest Decrease in Profits: (${greatest_profit_decrease})")
-
-# financial_report(months, total, avg, greatest_profit_increase, greatest_profit_decrease)
-
+# Print the summary of the Financial Analysis
+print()
+print("Financial Analysis")
+print("-------------------------------------------------")
+print(f"Total Months: {len(months)} ")
+print(f"Total: ${balance}")
+print(f"Average Change: ${avg}")
+print(
+    f"Greatest Increase in Profits: {greatest_profit_increase[0]} ({greatest_profit_increase[1]})")
+print(
+    f"Greatest Decrease in Profits: {greatest_profit_decrease[0]} ({greatest_profit_decrease[1]})")
