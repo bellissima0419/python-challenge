80a7538d46c177f18260f8228446e917d1a15047
marez111 <myrealgeorgia@gmail.com>
2019-03-19 13:23:27 -0400

    PyBank housekeeping

--- PyBank/main.py
+++ PyBank/main.py
@@ -28,10 +28,11 @@ for i in range(1, len(csv_list)):
     profit_loss_diff.append(
         [csv_list[i][0], csv_list[i][1] - csv_list[i - 1][1]])
 
-# calculate the max increase in profits. Calculation made with lambda on second item on the profit_loss_diff list, thus creating a list that contains both the month the year and the amount
+# calculate the max increase in profits. Calculation made with lambda on
+# second item on the profit_loss_diff list, thus creating a list that
+# contains both the month the year and the amount
 greatest_profit_increase = max(profit_loss_diff, key=lambda x: x[1])
 
-# print(f"greatest_profit_increase: {greatest_profit_increase}")
 
 # calculate the max decrease in profits
 greatest_profit_decrease = min(profit_loss_diff, key=lambda x: x[1])
@@ -40,47 +41,35 @@ avg = round(sum([x[1] for x in profit_loss_diff]) / len(profit_loss_diff), 2)
 
 # Create the summary of the Financial Analysis
 summary = {
-  "title": "Financial Analysis",
-  "underline": "-------------------------------------------------",
-  "Total Months": f":{len(months)}",
-  "Total": f"${balance}",
-  "Average Change": f"${avg}",
-  "Greatest Increase in Profits": f"{greatest_profit_increase[0]}: {greatest_profit_increase[1]}",
-  "Greatest Decrease in Profits": f"{greatest_profit_decrease[0]}: {greatest_profit_decrease[1]}"
+    "title": "Financial Analysis",
+    "underline": "-------------------------------------------------",
+    "Total Months": f":{len(months)}",
+    "Total": f"${balance}",
+    "Average Change": f"${avg}",
+    "Greatest Increase in Profits": f"{greatest_profit_increase[0]}: {greatest_profit_increase[1]}",
+    "Greatest Decrease in Profits": f"{greatest_profit_decrease[0]}: {greatest_profit_decrease[1]}"
 }
 
 
-# FinancialAnalysis = "Financial Analysis"
-# underline = "-------------------------------------------------"
-# total_months = f"Total Months: {len(months)}"
-# total = f"Total: ${balance}"
-# avg_change = f"Average Change: ${avg}"
-# greatest_inc_prof = f"Greatest Increase in Profits: {greatest_profit_increase[0]} ({greatest_profit_increase[1]})"
-# greatest_dec_prof = f"Greatest Decrease in Profits: {greatest_profit_decrease[0]} ({greatest_profit_decrease[1]})"
-
-# summary = [FinancialAnalysis, underline, total_months, total, avg_change,
-#            greatest_inc_prof, greatest_dec_prof]
-
-
 def financial_analysis(dict):
-    for k,v in dict.items():
-      if k == "title" or k == "underline":
-        print(v)
-      else: print(k,v)
+    print()
+    for k, v in dict.items():
+        if k == "title" or k == "underline":
+            print(v)
+        else:
+            print(k, v)
 
-# print("summary length", len(summary))
 
 financial_analysis(summary)
-
-
 
 # Open/create file in "write" mode ('w') and add the summary(analysis) to it
 file1 = './FinancialAnalysis.txt'
 with open(file1, 'w') as text:
-  for k, v in summary.items():
-      if k == "title" or k == "underline":
-        text.writelines([v, "\n"])
-      else: text.writelines([k,v, "\n"])
+    for k, v in summary.items():
+        if k == "title" or k == "underline":
+            text.writelines([v, "\n"])
+        else:
+            text.writelines([k, v, "\n"])
 
 # file2 = './FinancialAnalysis.csv'
 # with open(file2, 'w') as csv_file:
